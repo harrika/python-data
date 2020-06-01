cee5cbe7814da64c9968f18ce73e0c6e6968a3c9
henry kirya <henry.kirya@gmail.com>
2020-06-01 11:28:24 +0300

    Update special_missing_values.py
    adding part04 e14

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-
 import pandas as pd
 import numpy as np
 
 def special_missing_values():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep='\t')
+    df = df[~df['LW'].isin(['New', 'Re'])]
+    dff = df[df['Pos'] > df['LW'].astype(int)]
+    return dff
 
 def main():
-    return
+    dd = special_missing_values()
+    print(dd)
 
 if __name__ == "__main__":
     main()
+
